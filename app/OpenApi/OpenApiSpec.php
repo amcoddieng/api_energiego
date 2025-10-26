e74cc9f551b67e786aca1f115a506af2fde790de
amcoddieng <Dieng0097@gmail.com>
2025-10-26 14:11:11 +0000

    documentation api

---
+++ app/OpenApi/OpenApiSpec.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @OA\Info(
+ *     title="EnergieGo API",
+ *     version="1.0.0",
+ *     description="Documentation OpenAPI pour l'API EnergieGo"
+ * )
+ *
+ * @OA\Server(
+ *     url="/",
+ *     description="Serveur local"
+ * )
+ */
+class OpenApiSpec {}
